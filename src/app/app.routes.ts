1dc2f2ded33df19486f8d1c019c5988f886c1897
SudoCode76 <bo3321319@gmail.com>
2025-06-10 22:53:32 -0400

    añadir la integración CSS de Tailwind y configurar el enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {InicioComponent} from './components/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'inicio', component: InicioComponent},
+
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: '**', component: InicioComponent }
+
+];
